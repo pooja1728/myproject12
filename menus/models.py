5fac626e04627f0ab30fa020b4037356495fcd5c
pooja <31032324+manndeep1@users.noreply.github.com>
2017-09-26 20:15:56 +1000

    first commit

---
+++ menus/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+from restaurants.models import RestaurantLocation
+from django.core.urlresolvers import reverse
+
+class Item(models.Model):
+
+    # association
+   user         = models.ForeignKey(settings.AUTH_USER_MODEL)
+   restaurant   = models.ForeignKey(RestaurantLocation)
+   #Item stuff(
+   name        = models.CharField(max_length=120)
+   contents    = models.TextField(help_text='separate each item by comma')
+   excludes    = models.TextField(blank=True, null=True, help_text='Separate each item by comma')
+   public      = models.BooleanField(default=True)
+   timestamp   = models.DateTimeField(auto_now_add=True)
+   updated     = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+        return self.name
+    
+   def get_absolute_url(self):
+        # return f"/restaurants/{self.slug}"
+       return reverse('menus:detail', kwargs={'pk': self.pk})
+
+   class Meta:
+       ordering = ['-updated', '-timestamp']
+
+   def get_contents(self):
+       return self.contents.split(",")
+   def get_excludes(self):
+       return self.excludes.split(",")
+
+
+# Create your models here.
---
+++ restaurants/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import RestaurantLocation
+from .validators import validate_category
+
+class RestaurantCreateForm(forms.Form):
+    name        = forms.CharField()
+    location    = forms.CharField(required=False)
+    category    = forms.CharField(required=False)
+
+    def clean_name(self,):
+        name= self.cleaned_data.get("name")
+        if name == "Hello":
+            raise forms.ValidationError("Not a valid name")
+        return name
+
+class RestaurantLocationCreateForm(forms.ModelForm):
+    # category   = forms.CharField(required=False, validators=[validate_category])
+    # email = forms.EmailField()
+    class Meta:
+        model = RestaurantLocation
+        fields= [
+            'name',
+            'location',
+            'category',
+            
+        ]
+    def clean_name(self,):
+        name= self.cleaned_data.get("name")
+        if name == "Hello":
+            raise forms.ValidationError("Not a valid name")
+        return name
